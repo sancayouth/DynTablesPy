d3162748db2282b038dbc077687e68021232fa02
Cristian Sarmiento <cccsarmiento@gmail.com>
2015-12-05 05:25:59 -0300

    Add primary key field to DynAttributes model

--- app/models.py
+++ app/models.py
@@ -26,15 +26,18 @@ class DynAttribute(db.Model):
     __tablename__ = 'dynattributes'
 
     id = db.Column(db.String, primary_key=True)
+    pk = db.Column(db.Boolean)
     dyntable_id = db.Column(db.String, db.ForeignKey('dyntables.id'))
     attr_name = db.Column(db.String)
     display = db.Column(db.String)
     attr_type = db.Column(db.String(1))
     required = db.Column(db.Boolean)
 
-    def __init__(self, name, dynt_id, display, attr_t, required=False):
+    def __init__(self, name, dynt_id, display,
+                 attr_t, pk=False, required=False):
         self.id = dynt_id + ':' + name.lower().replace(' ', '_')
         self.attr_name = name.lower().replace(' ', '_')
+        self.pk = pk
         self.dyntable_id = dynt_id
         self.display_name = display
         self.attr_type = attr_t
@@ -42,4 +45,3 @@ class DynAttribute(db.Model):
 
     def __repr__(self):
         return '<DynAttribute (attribute_name=%r)>' % (self.id)
-
