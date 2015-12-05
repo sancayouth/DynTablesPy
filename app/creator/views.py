dde36b0c24125885b414b6e7d8b1f22a808cb757
Cristian Sarmiento <cccsarmiento@gmail.com>
2015-12-05 17:53:11 -0300

    Add table with attributes

--- app/creator/views.py
+++ app/creator/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template, redirect, url_for
-from .forms import DynTableForm
+from flask import Blueprint, render_template, redirect, url_for, request
+from .forms import DynTableForm, DynAttributeForm
 from app.extensions import db
-from app.models import DynTable
+from app.models import DynTable, DynAttribute
+from app.utils import formToDict, chboxtopy
 
 creator = Blueprint('creator', __name__, template_folder='templates',
                   url_prefix='/creator')
@@ -11,19 +12,32 @@ creator = Blueprint('creator', __name__, template_folder='templates',
 @creator.route('/add', methods=['GET', 'POST'])
 def add():
     form = DynTableForm()
+    detail_form = DynAttributeForm()
+    rform = request.form
     if form.validate_on_submit():
-        dynT = DynTable(form.id_.data)
+        dynT = DynTable(rform.get('id_'))
         db.session.add(dynT)
+        if len(rform) > 2:
+            #add detail
+            for i in range(1, len(rform) - 1):
+                attrDic = formToDict(rform.get('attr_' + str(i)))
+                attr = DynAttribute(
+                    attrDic.get('attr_name').replace('+',' '), dynT.id,
+                    attrDic.get('display').replace('+',' '),
+                    attrDic.get('attr_type'), chboxtopy(attrDic.get('pk')),
+                    chboxtopy(attrDic.get('required')))
+                db.session.add(attr)
         db.session.commit()
-        return redirect(url_for('creator.list_tables'))
+        return '1'
     return render_template('index.html', title='DynTable - Add Table',
-                           form=form)
+                           form=form, detail_form=detail_form, url='add')
 
 
 @creator.route('/edit/<id>', methods=['GET', 'POST'])
 def edit(id):
     dT = DynTable.query.get_or_404(id)
     form = DynTableForm()
+    detail_form = DynAttributeForm()
     if form.validate_on_submit():
         form.to_model(dT)
         db.session.add(dT)
@@ -31,7 +45,7 @@ def edit(id):
         return redirect(url_for('creator.list_tables'))
     form.from_model(dT)
     return render_template('index.html', title='DynTable - Edit Table ' + id,
-                           form=form)
+                           form=form, detail_form=detail_form)
 
 
 @creator.route('/')
---
+++ app/utils.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import urllib
+
+def formToDict(form):
+    fdict = {}
+    for l in urllib.unquote(form).split('&'):
+        val = l.split('=')
+        if val[1] != '':
+            fdict[val[0]] = val[1]
+    return fdict
+
+def chboxtopy(value):
+    return value == 'y'
